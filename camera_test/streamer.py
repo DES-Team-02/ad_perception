c5117c6b1cc9ad5f898fc9966f1899487ce53099
mczb <12190588@inha.edu>
2024-04-17 14:54:55 +0200

    Develop Streamer with OpenCV

---
+++ camera_test/streamer.py
@@ -0,0 +1,18 @@
+from jetcam.csi_camera import CSICamera
+
+import numpy as np
+import cv2 as cv
+
+camera = CSICamera(width=1280, height=720, capture_width=1280, capture_height=720, capture_fps=30)
+ 
+while True:
+    image = camera.read()
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    
+    cv.imshow('image', image)
+    cv.imshow('gray', gray)
+
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+cv.destroyAllWindows()
