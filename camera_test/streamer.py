1a5a2d5fefd84e57955378d216faf8e6221a106f
mczb <12190588@inha.edu>
2024-04-17 15:00:52 +0200

    Change resolution of source

--- camera_test/streamer.py
+++ camera_test/streamer.py
@@ -3,8 +3,8 @@ from jetcam.csi_camera import CSICamera
 import numpy as np
 import cv2 as cv
 
-camera = CSICamera(width=1280, height=720, capture_width=1280, capture_height=720, capture_fps=30)
- 
+camera = CSICamera(width=1280, height=720, capture_width=1920, capture_height=1080, capture_fps=30)
+
 while True:
     image = camera.read()
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
