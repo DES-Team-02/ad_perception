6b77459922ce450c9c3c95d65f40a43992cf8bd5
MC00614 <12190588@inha.edu>
2024-06-12 06:01:17 -0700

    Record Video

--- camera_test/recorder.py
+++ camera_test/recorder.py
@@ -3,7 +3,7 @@ from jetcam.csi_camera import CSICamera
 import numpy as np
 import cv2 as cv
 
-camera = CSICamera(width=1280, height=720, capture_width=1920, capture_height=1080, capture_fps=30)
+camera = CSICamera(width=1280, height=720, capture_width=1280, capture_height=720, capture_fps=30)
 
 fourcc = cv.VideoWriter_fourcc(*'XVID')
 out = cv.VideoWriter('new_output.avi', fourcc, 15, (1280, 720))
